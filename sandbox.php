286940b6f3d9a429e09923919e2c7cbb0439b015
marshallAndrew <kontakt.marcinwojcik@gmail.com>
2019-09-26 15:40:56 +0200

    Add getter and setter function

--- sandbox.php
+++ sandbox.php
@@ -2,8 +2,8 @@
 
 	class User {
 
-		public $email;
-		public $name;
+		private $email;
+		private $name;
 
 		public function __construct($name, $email) {
 			$this->email = $email;
@@ -14,6 +14,19 @@
 			//echo "the user logged in";
 			echo $this->name . 'logged in';
 		}
+
+		public function getName() {
+			return $this->name;
+		}
+
+		public function setName($name) {
+			if (is_string($name) && strlen($name) > 1) {
+				$this->name = $name;
+				return "name has been updated to $name";
+			} else {
+				return 'not a valid name';
+			}
+		}
 	}
 
 	// $userOne = new User();
@@ -22,9 +35,11 @@
 	// echo $userOne->email;
 
 	$userTwo = new User('yoshi', 'yoshi@netninja.com');
-	// echo $userTwo->name;
-	// echo $userTwo->email;
+	
+	//echo $userTwo->getName();
+	//echo $userTwo->setName(50);
 
-	$userTwo->login();
+	echo $userTwo->setName('martin') . '</br>';
+	echo $userTwo->getName();
 
 ?>
